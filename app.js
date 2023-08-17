cf4ec3a3effcbf8a085cfbdacb727e333835dbc3
yiching-hsu <ychsu7149@gmail.com>
2023-08-17 21:06:35 +0800

    add getRestaurants api

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const passport = require('./config/passport')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
 const { getUser } = require('./helpers/auth-helpers') // 引入自定義的 auth-helpers
 
-const { pages } = require('./routes')
+const { pages, apis } = require('./routes')
 
 // const routes = require('./routes')
 
@@ -40,6 +40,7 @@ app.use((req, res, next) => {
   next()
 })
 
+app.use('/api', apis)
 app.use(pages)
 
 app.listen(port, () => {
